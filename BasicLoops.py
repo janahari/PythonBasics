08c735b0d46b7948f19d1137a724a4ee7ed66945
janahari <jana.hariprasad525@gmail.com>
2025-07-20 07:20:55 +0530

    Updated with forloop

--- BasicLoops.py
+++ BasicLoops.py
@@ -1,1 +1,8 @@
 print("Welcome to Python Practise")
+
+name = input("Enter your name :")
+print("Hello, "+ name + "!")
+
+fruits = ["Apple", "Banana","Kiwi","Jackfruit","Blueberry"]
+for fruit in fruits:
+    print(fruit)
