3a583de91736eab069b9d7763d367aa24e0e62a4
janahari <jana.hariprasad525@gmail.com>
2025-07-31 22:17:32 +0530

    Update

--- functionSamples.py
+++ functionSamples.py
@@ -20,4 +20,34 @@ def print_num(*args):
     for val in args:
         print(val)
     
-print_num(3,4,5,"Hello")
+#print_num(3,4,5,"Hello")
+
+def sayHello(name="Gues"):
+    print(f"Hello Welcoem to {name}")
+
+#sayHello("Hari")
+
+def print_details(**kedetails):
+    for key, val in kedetails.items():
+      print(f"{key}:{val}")
+    
+#print_details(Name="Hari",Age=32,Status="single")
+
+def print_detailArgs(*args, ** details):
+    for val in args:
+        print(f"Positinal argumens{val}")
+    for key,val in details.items():
+        print(f"The Key:{key} and value is:{val}")
+
+print_detailArgs(3,4,5,"Hello",Name="Hari",Age=32,Status="single")
+
+## Return statement 
+def multiply(a,b):
+    return a * b
+
+#Python can return multiple 
+def multiVals(a, b):
+    return a,a*b
+
+print(multiply(2,3))
+print(multiVals(3,4))
